0c14de68dcc62fdb01bda6f570b81f0532aab99b
Martin_vg <Martinnvg@gmail.com>
2023-06-21 19:52:16 -0700

    Agrega Ejercicio 3

---
+++ Clase 3/Clase3.py
@@ -0,0 +1,5 @@
+''' Implemente una de las siguientes tres estructuras de datos en python (Pilas, colas árboles) con lassiguientes funciones básicas:
+    • agregar
+    • eliminar
+    • buscar'''
+
