42b520a870a53e0d5cdcba8fcd5c7c429968b820
Martin_vg <Martinnvg@gmail.com>
2023-06-21 19:55:13 -0700

    Agrega Ejercicio Clase 5

--- Clase 4/lecturaCsv.py
+++ Clase 4/lecturaCsv.py
@@ -1,21 +1,24 @@
-from sys import argv
-import sys
+from sys import argv, exit
 import csv
 import re
 
 if len(argv) < 3:
-    sys.exit("Missing arguments")
+    exit("Missing arguments")
 
 if not(re.match(r'.+\.csv$', argv[1])):
     print(argv[1])
     sys.exit("Fisrt argument must be a csv file")
 
+i=0
 with open(argv[1]) as csvfile:
     reader = csv.DictReader(csvfile)
     for row in reader:
+        i+=1 
         try:
             print(row[argv[2]])
         except:
             sys.exit("Column not found")
-
+with open(argv[1], 'a') as csvfile:
+    writer = csv.writer(csvfile)
+    writer.writerow([1] * i)
 
